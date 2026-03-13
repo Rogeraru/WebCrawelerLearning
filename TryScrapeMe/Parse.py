9dcc5007f5be0c47ed5325233b64480f9e203202
Roger Huang <rager1222@gmail.com>
2026-03-13 13:55:56 +0800

    parse

---
+++ TryScrapeMe/Parse.py
@@ -0,0 +1,41 @@
+#https://tryscrapeme.com/web-scraping-practice/beginner/parse
+
+import requests
+import sys
+
+from lxml import etree
+
+url = 'https://tryscrapeme.com/web-scraping-practice/beginner/parse'
+
+try:
+    response = requests.get(url)
+except requests.exception.RequestException as e:
+    print(f'failed with exception {e}')
+    sys.exit()
+
+if response.status_code != 200:
+    print(f'failed with status code {response.status_code}')
+    sys.exit()
+
+html = response.text
+root = etree.HTML(html)
+
+#Dom:
+# <tr>
+#     <td><h4>The Brain That Changes Itself</h4></td>
+#     <td>Norman Doidge</td>
+#     <td>4</td>
+#     <td><span>$</span>11.03</td>
+# </tr>
+
+lst = []
+body = root.xpath('//tbody')
+
+for tbody in body:
+    trs = tbody.xpath('./tr')
+    for tr in trs:
+        target = tr.xpath('./td[4]/text()')
+        price = float(target[0])
+        lst.append(price)
+total_price = sum(lst)
+print(f'total price is {total_price}')
